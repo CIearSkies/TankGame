3843b62b7fb87bec2f454f50ecb11725597b0d55
Thomas Brouwers <tmbrouwers@gmail.com>
2017-10-22 14:37:48 +0200

    added everything from communication

--- tankgame/tankgame/tankgame/Entities/Tank.cs
+++ tankgame/tankgame/tankgame/Entities/Tank.cs
@@ -15,9 +15,11 @@ namespace tankgame.Entities
     {
         Boolean shoot = true;
         System.Timers.Timer timer;
+        Game1 game;
         Xbox360GamePad mGamePad;
         private void CustomInitialize()
         {
+            game = (Game1)FlatRedBallServices.Game;
             mGamePad = InputManager.Xbox360GamePads[0];
             KeyboardButtonMap buttonMap = new KeyboardButtonMap();
 
@@ -57,6 +59,16 @@ namespace tankgame.Entities
             MovementActivity();
             TurningActivity();
             ShootingActivity();
+            if (game.Player == "player1")
+            {
+                GivePosition();
+                GiveRotation();
+            }
+            else if (game.Player == "player2")
+            {
+                UpdatePosition();
+                UpdateRotation();
+            }
         }
 
         public void makeTimer()
@@ -107,6 +119,27 @@ namespace tankgame.Entities
         void TurningActivity()
         {
             this.RotationZVelocity = -mGamePad.LeftStick.Position.X * 0.7f;
+        }
+
+        void GivePosition()
+        {
+
+            game.Position = this.Position;
+        }
+
+        void GiveRotation()
+        {
+            game.Rotation = this.RotationZ;
+        }
+
+        void UpdatePosition()
+        {
+            this.Position = game.Position;
+        }
+
+        void UpdateRotation()
+        {
+            this.RotationZ = game.Rotation;
         }
     }
 }
--- tankgame/tankgame/tankgame/Game1.cs
+++ tankgame/tankgame/tankgame/Game1.cs
@@ -11,12 +11,21 @@ using System.Linq;
 
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Net.Sockets;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace tankgame
 {
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
+        NetworkStream stream;
+
+        public Vector3 Position { get; set; }
+        public float Rotation { get; set; }
+        public string Player { get; set; }
 
         public Game1() : base ()
         {
@@ -43,22 +52,74 @@ namespace tankgame
 
         protected override void Initialize()
         {
-			#if IOS
+#if IOS
 			var bounds = UIKit.UIScreen.MainScreen.Bounds;
 			var nativeScale = UIKit.UIScreen.MainScreen.Scale;
 			var screenWidth = (int)(bounds.Width * nativeScale);
 			var screenHeight = (int)(bounds.Height * nativeScale);
 			graphics.PreferredBackBufferWidth = screenWidth;
 			graphics.PreferredBackBufferHeight = screenHeight;
-			#endif
-		
-            FlatRedBallServices.InitializeFlatRedBall(this, graphics);
-            FlatRedBallServices.GraphicsOptions.TextureFilter = TextureFilter.Point;
-            CameraSetup.SetupCamera(SpriteManager.Camera, graphics);
-			GlobalContent.Initialize();
-			FlatRedBall.Screens.ScreenManager.Start(typeof(tankgame.Screens.GameScreen));
+#endif
 
-            base.Initialize();
+            try
+            {
+
+                Int32 port = 13000;
+                TcpClient client = new TcpClient("127.0.0.1", port);
+
+                stream = client.GetStream();
+
+                byte[] preBuffer = new Byte[4];
+                stream.Read(preBuffer, 0, 4);
+                int lenght = BitConverter.ToInt32(preBuffer, 0);
+                byte[] buffer = new Byte[lenght];
+                int totalReceived = 0;
+                while (totalReceived < lenght)
+                {
+                    int receivedCount = stream.Read(buffer, totalReceived, lenght - totalReceived);
+                    totalReceived += receivedCount;
+                }
+                JObject Json = JObject.Parse(Encoding.UTF8.GetString(buffer));
+
+                if (Json.GetValue("id").ToString() == "player1")
+                {
+                    Player = "player1";
+                }
+                else if (Json.GetValue("id").ToString() == "player2")
+                {
+                    Player = "player2";
+                }
+                preBuffer = new Byte[4];
+                stream.Read(preBuffer, 0, 4);
+                lenght = BitConverter.ToInt32(preBuffer, 0);
+                buffer = new Byte[lenght];
+                totalReceived = 0;
+                while (totalReceived < lenght)
+                {
+                    int receivedCount = stream.Read(buffer, totalReceived, lenght - totalReceived);
+                    totalReceived += receivedCount;
+                }
+                Json = JObject.Parse(Encoding.UTF8.GetString(buffer));
+
+                if (Json.GetValue("id").ToString() == "start")
+                {
+                    FlatRedBallServices.InitializeFlatRedBall(this, graphics);
+                    FlatRedBallServices.GraphicsOptions.TextureFilter = TextureFilter.Point;
+                    CameraSetup.SetupCamera(SpriteManager.Camera, graphics);
+                    GlobalContent.Initialize();
+                    FlatRedBall.Screens.ScreenManager.Start(typeof(tankgame.Screens.GameScreen));
+                    base.Initialize();
+                }
+
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
         }
 
 
@@ -69,6 +130,19 @@ namespace tankgame
             FlatRedBall.Screens.ScreenManager.Activity();
 
             base.Update(gameTime);
+            BinaryFormatter formatter = new BinaryFormatter();
+            if (Player == "player1")
+            {
+                formatter.Serialize(stream, Position);
+                formatter.Serialize(stream, Rotation);
+
+
+            }
+            else if (Player == "player2")
+            {
+                Position = (Vector3)formatter.Deserialize(stream);
+                Rotation = (float)formatter.Deserialize(stream);
+            }
         }
 
         protected override void Draw(GameTime gameTime)
